6120b510b041db6cca5c001dfc719d092bb554a2
Carlos Montes <carlos.montes@karasustudio.com>
2026-02-21 20:04:01 -0300

    puzzle 3 (nightmare)

---
+++ source/creature/projection_attack.gd
@@ -0,0 +1,18 @@
+class_name ProjectionAttack extends Interactable
+
+@export var velocity : float = 0.01
+
+signal on_hit(was_fake: bool)
+
+var _is_fake : bool = false
+
+func _physics_process(delta: float) -> void:
+	position = Vector3(position.x, position.y, position.z + (velocity * delta))
+
+func mark_as_fake() -> void:
+	_is_fake = true
+	get_node("./Sprite").modulate.a = 0.8
+	
+func _on_interact():
+	on_hit.emit(_is_fake)
+	
---
+++ source/creature/puzzle_3.gd
@@ -0,0 +1,88 @@
+class_name Puzzle_3 extends Node3D
+
+var _moves : int = 1
+var _eliminated_attacks : int = 0
+var _cancelled_hit := false
+var timer: SceneTreeTimer
+@export var key_item: KeyItem
+@export var attack: PackedScene
+
+var should_stop := false
+var y_positions : Array[float] = [0.2, 0.5, 0.8]
+var current_attacks : Array[ProjectionAttack] = []
+var emitted_attacks: Array[AttackLine] = []
+
+func _ready() -> void:
+	EventBusSingleton.on_event.connect(_on_event)
+	
+	
+func _on_event(event: Object) -> void:
+	if event is MoveToNextSpotEvent: return _handle_player_movement()
+	
+
+func _handle_player_movement() -> void:
+	_moves += 1
+	if _moves == 4:
+		get_tree().create_timer(2.0).timeout.connect(_start_puzzle)
+		
+func _start_puzzle() -> void:
+	if should_stop:
+		return
+		
+	_spawn_attack()	
+	
+func _validate_attacks() -> void:
+	_eliminated_attacks += 1
+	print(_eliminated_attacks)
+	if _eliminated_attacks == 10:
+		should_stop = true
+		key_item.visible = true
+		queue_free.call_deferred()
+	else:
+		await get_tree().create_timer(1.0).timeout
+		_start_puzzle()
+	
+func _on_hit_projection(was_fake: bool) -> void:
+	_cancelled_hit = true
+	timer.timeout.disconnect(_show_fail_message)
+	for projection in current_attacks:
+		projection.queue_free.call_deferred()
+		
+	if !was_fake:
+		_validate_attacks()
+		_show_hitted_message()
+	else:
+		_show_fake_message()
+				
+func _show_fake_message() -> void:
+	pass
+	
+func _show_hitted_message() -> void:
+	pass
+	
+func _show_fail_message() -> void:
+	_cancelled_hit = true
+	_start_puzzle()
+	
+func _spawn_attack() -> void:
+	_cancelled_hit = false
+	var attack_1 : ProjectionAttack = attack.instantiate()
+	var attack_2 : ProjectionAttack = attack.instantiate()
+	current_attacks = [attack_1, attack_2]
+	attack_1.position = Vector3(0.5, 0.7, 0.8)
+	attack_2.position = Vector3(-0.5, 0.7, 0.8)
+	
+	attack_1.velocity = -1
+	attack_2.velocity = -1
+	
+	attack_1.on_hit.connect(_on_hit_projection)
+	attack_2.on_hit.connect(_on_hit_projection)
+	
+	add_sibling.call_deferred(attack_1)
+	add_sibling.call_deferred(attack_2)
+	
+	var fake_one = [attack_1, attack_2].pick_random()
+	fake_one.mark_as_fake()
+	
+	timer = get_tree().create_timer(4.0)
+	timer.timeout.connect(_show_fail_message)
