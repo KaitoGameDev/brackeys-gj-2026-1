36738a6001b6f2ecb072afec212d4e77428fcbf0
Carlos Montes <carlos.montes@karasustudio.com>
2026-02-21 20:48:11 -0300

    activated occlusion culling (?)

--- source/event_bus/event_bus.gd
+++ source/event_bus/event_bus.gd
@@ -2,5 +2,8 @@ class_name EventBus extends Node
 
 signal on_event(event: Object)
 
+func _ready() -> void:
+	get_tree().root.use_occlusion_culling = true
+
 func send_event(event: Object) -> void:
 	on_event.emit(event)
