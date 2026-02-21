2505541519c5ec6d325e47357c9721c815c5b74a
Lascario Pacheco <lascariopacheco@hotmail.com>
2026-02-21 11:42:15 -0300

    feat: added interactable base class

---
+++ source/interactions/interactable.gd
@@ -0,0 +1,35 @@
+class_name Interactable
+extends Node3D
+
+@export var detector: Area3D
+@export var hover_label: String
+
+var _is_over_item = false
+
+func _ready() -> void:
+	detector.mouse_entered.connect(_on_mouse_entered)
+	detector.mouse_exited.connect(_on_mouse_exited)
+	
+func _on_mouse_entered() -> void:
+	_is_over_item = true
+	var cursor_event := ChangeCursorEvent.new()
+	cursor_event.cursor_type = ChangeCursorEvent.CursorType.INTERACTABLE
+	
+	if hover_label != null:
+		cursor_event.label = hover_label
+	
+	EventBusSingleton.send_event(cursor_event)
+	
+func _on_mouse_exited() -> void:
+	_is_over_item = false
+	var cursor_event := ChangeCursorEvent.new()
+	cursor_event.cursor_type = ChangeCursorEvent.CursorType.NORMAL
+	cursor_event.label = ''
+	EventBusSingleton.send_event(cursor_event)
+	
+func _process(_delta: float) -> void:
+	if Input.is_mouse_button_pressed(MouseButton.MOUSE_BUTTON_LEFT) and _is_over_item:
+		_on_interact()
+		
+func _on_interact():
+	print("Interactable clicked")
--- source/props/key_item.gd
+++ source/props/key_item.gd
@@ -1,33 +1,9 @@
-class_name KeyItem extends Node3D
+class_name KeyItem extends Interactable
 
-@onready var detector: Area3D = $Detector
-
-var _is_over_item : bool = false
-
-func _ready() -> void:
-	detector.mouse_entered.connect(_on_mouse_entered)
-	detector.mouse_exited.connect(_on_mouse_exited)
-	
-func _on_mouse_entered() -> void:
-	_is_over_item = true
+func _on_interact(): 
 	var cursor_event := ChangeCursorEvent.new()
-	cursor_event.cursor_type = ChangeCursorEvent.CursorType.INTERACTABLE
-	cursor_event.label = 'Take'
-	EventBusSingleton.send_event(cursor_event)
-	
-func _on_mouse_exited() -> void:
-	_is_over_item = false
-	var cursor_event := ChangeCursorEvent.new()
-	cursor_event.cursor_type = ChangeCursorEvent.CursorType.NORMAL
+	cursor_event.cursor_type = ChangeCursorEvent.CursorType.TAKE
 	cursor_event.label = ''
 	EventBusSingleton.send_event(cursor_event)
-	
-func _process(_delta: float) -> void:
-	
-	if Input.is_mouse_button_pressed(MouseButton.MOUSE_BUTTON_LEFT) and _is_over_item:
-		var cursor_event := ChangeCursorEvent.new()
-		cursor_event.cursor_type = ChangeCursorEvent.CursorType.TAKE
-		cursor_event.label = ''
-		EventBusSingleton.send_event(cursor_event)
-		EventBusSingleton.send_event(MoveToNextSpotEvent.new())
-		queue_free.call_deferred()
+	EventBusSingleton.send_event(MoveToNextSpotEvent.new())
+	queue_free.call_deferred()
