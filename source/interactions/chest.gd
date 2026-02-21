14354abffcc6534bffe20a13e5c09e4f87fed91f
Lascario Pacheco <lascariopacheco@hotmail.com>
2026-02-21 12:22:26 -0300

    feat: add puzzle 1 and 2 to the dungeon level

---
+++ source/interactions/chest.gd
@@ -0,0 +1,18 @@
+extends Interactable
+
+@export var chest_lid_object: Node3D
+@export var item_container: Node3D
+
+# the item inside the chest - if empty won't spawn anything
+@export var item: PackedScene
+
+func _on_interact():
+	var lid_tween = get_tree().create_tween()
+	lid_tween.tween_property(chest_lid_object, "rotation:x", deg_to_rad(-80.0), 1.0)
+	
+	if item != null:
+		var item_ref = item.instantiate()
+		item_container.add_child(item_ref)
+		
+		var container_tween = get_tree().create_tween()
+		container_tween.tween_property(item_container, "position:y", 1.0, 2.0)
