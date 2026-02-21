61b00ca27702aeb5420240e2a67ed07dbe610903
Carlos Montes <carlos.montes@karasustudio.com>
2026-02-21 20:29:23 -0300

    improve puzzle 3 (nightmare)

--- source/creature/puzzle_3.gd
+++ source/creature/puzzle_3.gd
@@ -4,8 +4,13 @@ var _moves : int = 1
 var _eliminated_attacks : int = 0
 var _cancelled_hit := false
 var timer: SceneTreeTimer
+var message_timer: SceneTreeTimer
+
 @export var key_item: KeyItem
 @export var attack: PackedScene
+
+@onready var message_container: Control = $Control/Background
+@onready var messages_label: RichTextLabel = $Control/Background/Messages
 
 var should_stop := false
 var y_positions : Array[float] = [0.2, 0.5, 0.8]
@@ -53,16 +58,30 @@ func _on_hit_projection(was_fake: bool) -> void:
 		_show_hitted_message()
 	else:
 		_show_fake_message()
-				
+		get_tree().create_timer(2.0).timeout.connect(_start_puzzle)
+
+func _show_message(text: String) -> void:
+	if message_timer:
+		message_timer.timeout.disconnect(_disappear_message)
+	message_container.visible = true
+	messages_label.text = text
+	message_timer = get_tree().create_timer(2.0)
+	message_timer.timeout.connect(_disappear_message)
+		
 func _show_fake_message() -> void:
-	pass
+	_show_message('You Fool!, that was fake\n[shake]HAHAHAHA[/shake]')
+	
 	
 func _show_hitted_message() -> void:
-	pass
+	_show_message('[shake]AHHH!! I will get you![/shake]')
 	
 func _show_fail_message() -> void:
 	_cancelled_hit = true
 	_start_puzzle()
+	_show_message('[shake]JE je JE! part of you is mine![/shake]')
+	
+func _disappear_message() -> void:
+	message_container.visible = false
 	
 func _spawn_attack() -> void:
 	_cancelled_hit = false
@@ -72,8 +91,8 @@ func _spawn_attack() -> void:
 	attack_1.position = Vector3(0.5, 0.7, 0.8)
 	attack_2.position = Vector3(-0.5, 0.7, 0.8)
 	
-	attack_1.velocity = -1
-	attack_2.velocity = -1
+	attack_1.velocity = -0.85
+	attack_2.velocity = -0.85
 	
 	attack_1.on_hit.connect(_on_hit_projection)
 	attack_2.on_hit.connect(_on_hit_projection)
@@ -84,5 +103,5 @@ func _spawn_attack() -> void:
 	var fake_one = [attack_1, attack_2].pick_random()
 	fake_one.mark_as_fake()
 	
-	timer = get_tree().create_timer(4.0)
+	timer = get_tree().create_timer(2.5)
 	timer.timeout.connect(_show_fail_message)
