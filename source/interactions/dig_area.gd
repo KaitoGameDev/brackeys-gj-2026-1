a2356386dea326f41d717017e24eec3e8ab499ae
Lascario Pacheco <lascariopacheco@hotmail.com>
2026-02-21 21:58:04 -0300

    feat: add digging puzzle

---
+++ source/interactions/dig_area.gd
@@ -0,0 +1,15 @@
+extends Interactable
+
+@export var buried_item: PackedScene
+@export var item_container: Node3D
+@export var area_model: Node3D
+
+func _on_interact():
+	area_model.visible = false
+	if buried_item != null:
+		var item_ref = buried_item.instantiate()
+		
+		item_container.add_child(item_ref)
+		
+		var container_tween = get_tree().create_tween()
+		container_tween.tween_property(item_container, "position:y", 0.8, 2.0)
