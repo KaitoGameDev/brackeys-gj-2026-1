bcf10c110cdf3e5df05c7b9c3c6721fb0a0956a2
Carlos Montes <carlos.montes@karasustudio.com>
2026-02-21 21:13:13 -0300

    possible improve

--- source/event_bus/event_bus.gd
+++ source/event_bus/event_bus.gd
@@ -2,8 +2,5 @@ class_name EventBus extends Node
 
 signal on_event(event: Object)
 
-func _ready() -> void:
-	get_tree().root.use_occlusion_culling = true
-
 func send_event(event: Object) -> void:
 	on_event.emit(event)
--- source/room/room.gd
+++ source/room/room.gd
@@ -10,6 +10,8 @@ var _halls: Array[Node3D] = []
 var _current_hall := 0
 
 func _ready() -> void:
+	get_tree().root.use_occlusion_culling = true
+	
 	for hall in center.get_children():
 		_halls.push_back(hall)
 	EventBusSingleton.send_event(GameStartedEvent.new())
@@ -28,11 +30,19 @@ func _on_move_to_next_spot() -> void:
 func _on_playern_movement_end() -> void:
 	if sections.size() == 0: return
 	
-	var old_section : Node3D = _halls[_current_hall].get_child(0)
-	var section = sections.pop_front().instantiate()
-	_halls[_current_hall].add_child.call_deferred(section)
-	section.position = old_section.position
-	old_section.queue_free.call_deferred()
 	_current_hall += 1
+	
+	if _current_hall == 3:
+		for i in 3:
+			var old_section : Node3D = _halls[i].get_child(0)
+			var section = sections.pop_front().instantiate()
+			_halls[i].add_child.call_deferred(section)
+			section.position = old_section.position
+			old_section.queue_free.call_deferred()
 	if _current_hall == 4:
+		var old_section : Node3D = _halls[3].get_child(0)
+		var section = sections.pop_front().instantiate()
+		_halls[3].add_child.call_deferred(section)
+		section.position = old_section.position
+		old_section.queue_free.call_deferred()
 		_current_hall = 0
