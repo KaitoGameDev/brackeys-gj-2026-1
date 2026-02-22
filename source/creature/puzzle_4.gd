7dc77fc5fba612eb31ac94706fb706a6eb0322f0
Carlos Montes <carlos.montes@karasustudio.com>
2026-02-21 23:05:35 -0300

    puzzle 4 / windows build

---
+++ source/creature/puzzle_4.gd
@@ -0,0 +1,43 @@
+extends Control
+
+var _moves : int = 1
+var message_timer: SceneTreeTimer
+
+@export var key_item: KeyItem
+@export var creature: Creature
+
+@onready var message_container: Control = $Background
+@onready var messages_label: RichTextLabel = $Background/Messages
+
+func _ready() -> void:
+	_start_puzzle()
+	EventBusSingleton.on_event.connect(_on_event)
+	
+	
+func _on_event(event: Object) -> void:
+	if event is MoveToNextSpotEvent: return _handle_player_movement()
+	
+
+func _handle_player_movement() -> void:
+	_moves += 1
+	if _moves == 4:
+		get_tree().create_timer(2.0).timeout.connect(_start_puzzle)
+		
+func _start_puzzle() -> void:
+	_show_message('[shake]You maybe win this time[/shake]')
+	await get_tree().create_timer(2.0).timeout
+	_show_message('[shake]But I\'m part of you\neven if you like or not[/shake]')
+	await get_tree().create_timer(4.0).timeout
+	_show_message('[shake]I\'ll be waiting for you[/shake]')
+	await get_tree().create_timer(3.0).timeout
+	creature.disappear()
+	key_item.visible = true
+	_disappear_message()
+
+func _show_message(text: String) -> void:
+	message_container.visible = true
+	messages_label.text = text
+
+		
+func _disappear_message() -> void:
+	message_container.visible = false
