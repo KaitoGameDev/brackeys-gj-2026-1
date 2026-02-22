9323695a240977607ffca2ee0913848785700042
Carlos Montes <carlos.montes@karasustudio.com>
2026-02-21 22:10:51 -0300

    add tween in keyitem

--- source/props/key_item.gd
+++ source/props/key_item.gd
@@ -3,6 +3,16 @@ class_name KeyItem extends Interactable
 @export var next_puzzle_limit_time: float = 20.0
 @export var open_door_number: int = 0
 
+func _ready() -> void:
+	super._ready()
+	_tween()
+
+func _tween() -> void:
+	var tween := create_tween()
+	tween.tween_property(self, 'position:y', position.y - 0.1, 0.5)
+	tween.tween_property(self, 'position:y', position.y, 0.5)
+	tween.finished.connect(_tween)
+
 func _on_interact(): 
 	var cursor_event := ChangeCursorEvent.new()
 	cursor_event.cursor_type = ChangeCursorEvent.CursorType.TAKE
